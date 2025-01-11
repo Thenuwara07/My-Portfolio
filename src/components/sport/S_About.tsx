83d3730c94665142476fff216cd7bf4095971618
Thenuwara <pramukhapoo@gmail.com>
2025-01-11 23:26:12 +0530

    add my timline deatiils and upppdate cv

--- src/components/sport/S_About.tsx
+++ src/components/sport/S_About.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">
+      <h2 className="mt-20 text-center text-4xl">
         <motion.span
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
